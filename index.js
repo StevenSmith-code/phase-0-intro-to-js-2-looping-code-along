40443d38dd3785fbba330b5d7ce5197dc3d574fc
Steven <stevensmith3211@gmail.com>
2022-10-27 18:30:10 -0500

    commit tree 5c8c1555d851e2a402ee647c396062257c46d91d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const writeCards = (arr, event) =>{
+    const newArr = []
+    for(let i = 0; i < arr.length; i++){
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArr
+} 
+
+const countDown = (positiveInt) => {
+    while(positiveInt > -1){
+        console.log(positiveInt)
+        positiveInt--
+    }
+}
